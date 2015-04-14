4f8652b2a56579474e84ca4cb904b8ea5f76fc2d
justindlangley <justindlangley@gmail.com>
2015-04-14 11:40:33 -0400

    Changed name of classes

---
+++ TDDMorningLab/BankAccount.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDMorningLab
+{
+    public class BankAccount
+    {
+        public int accountNumber { get; set; }
+        public string accountName { get; set; }
+        public double accountBalance {get; set;}
+
+        BankAccount customer1 = new BankAccount();
+            
+    }
+}
